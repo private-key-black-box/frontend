fcad67402068e62fa2ac6fa1314ddf74303c42a4
Thomas Turek <thomasturekpersonal@gmail.com>
2024-05-19 09:57:04 +0200

    cleaning up top left

--- apps/web/components/header.tsx
+++ apps/web/components/header.tsx
@@ -18,25 +18,8 @@ export function Header() {
         <div className="flex items-center space-x-4">
           <div className="flex flex-col items-start">
             <p className="text-lg font-semibold">Hi, Harry! 😊</p>
-            <p className="text-sm text-gray-500">Your Balance...</p>
-            <p className="text-2xl font-bold">1 000.0 MINA</p>
           </div>
           <div className="flex items-center space-x-4">
-            <div className="flex items-center space-x-1">
-              <WalletIcon className="h-6 w-6" />
-              <div className="flex flex-col items-start">
-                <p className="text-sm">Light Wallet Connected</p>
-                <p className="text-sm text-gray-500">
-                  {truncateMiddle('0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', 10, 10, "...")}
-                </p>
-              </div>
-            </div>
-            <Button variant="outline" size="icon">
-              <ChevronsRight className="h-5 w-5" />
-            </Button>
-            <Button variant="outline" size="icon">
-              <PiggyBank className="h-5 w-5" />
-            </Button>
           </div>
         </div>
       </div>
