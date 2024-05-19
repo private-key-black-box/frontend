89dfb0600e510fc90b5dd44cd82b4e23c5163a95
Thomas Turek <thomasturekpersonal@gmail.com>
2024-05-19 10:01:58 +0200

    cleanup

--- apps/web/components/header.tsx
+++ apps/web/components/header.tsx
@@ -1,19 +1,13 @@
 "use client";
 import { cn } from "@/lib/utils";
-import { Button } from "./ui/button";
-import { ChevronsRight, PiggyBank, Wallet as WalletIcon } from "lucide-react";
 import { useEffect, useState } from "react";
-import { GeistMono } from "geist/font/mono";
-import { Balance } from "./ui/balance";
-import { USDBalance } from "./ui/usd-balance";
 // @ts-ignore
 import truncateMiddle from "truncate-middle";
 
-
 export function Header() {
   return (
-    <div className="flex justify-center pt-4">
-      <div className="flex basis-11/12 items-center justify-start space-x-4 py-4">
+    <header className="flex justify-center pt-4">
+      <div className="flex w-11/12 items-center justify-start space-x-4 py-4">
         <img className="max-h-16" src="logo.png" alt="Logo" />
         <div className="flex items-center space-x-4">
           <div className="flex flex-col items-start">
@@ -21,6 +15,6 @@ export function Header() {
           </div>
         </div>
       </div>
-    </div>
+    </header>
   );
 }
